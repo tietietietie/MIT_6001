693aee9842367fce5f9fbaa304f3d06a65587908
ZhangTie <zhangtie@hust.edu.cn>
2019-07-14 15:49:17 +0800

    codes for PSET0

---
+++ PY0.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 14 15:32:36 2019
+
+@author: zhang
+"""
+x=int(input("Enter number x: "))
+y=int(input("Enter number y: "))
+print("x**y =  "+str(x**y))
+import numpy
+print("log(x) = "+str(int(numpy.log2(x))))
